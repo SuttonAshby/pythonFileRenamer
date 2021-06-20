94597f13ebc13be226aa94bfec1c604a65c99efc
Sutton Ashby <sutton.ashby@gmail.com>
2021-06-20 16:29:29 -0400

    started python renamer project

---
+++ fileRenamer.py
@@ -0,0 +1,15 @@
+# Working on a script to rename files based in part on:
+# https://www.marsja.se/rename-files-in-python-a-guide-with-examples-using-os-rename/
+
+import os
+
+#PSEUDO CODE
+
+# Get list of all files in specific directory put into an array
+# For loop for each file:
+#   get current name and store as a variable
+#   create new name based on information in old name
+#   use os.rename([current name and location], [new name and location])
+
+#extras would include a result print at the end for total conversions and
+# any errors that occurred
